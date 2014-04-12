524b46be96d60a572e00a1f00a5a2abd286ae843
Ash Wilson <smashwilson@gmail.com>
2014-04-11 21:02:58 -0400

    Read and write from a Hash.

--- lib/cashmonies/transaction.rb
+++ lib/cashmonies/transaction.rb
@@ -12,12 +12,21 @@ module Cashmonies
 
     def to_h
       {
-        uuid: @uuid,
-        timestamp: @timestamp.strftime('%Y-%m-%d'),
-        code: @code.to_s,
-        description: @description,
-        amount: @amount
+        'uuid' => @uuid,
+        'timestamp' => @timestamp.strftime('%Y-%m-%d'),
+        'code' => @code.to_s,
+        'description' => @description,
+        'amount' => @amount
       }
+    end
+
+    def self.from_h(hash)
+      Transaction.new(hash['uuid']).tap do |t|
+        t.timestamp = Time.parse(hash['timestamp'])
+        t.code = hash['code'].to_sym
+        t.description = hash['description']
+        t.amount = hash['amount'].to_i
+      end
     end
   end
 
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -9,14 +9,29 @@ describe 'Transaction' do
     t.amount = 1000
 
     t.to_h.should == {
-      uuid: t.uuid,
-      timestamp: '2014-01-02',
-      code: 'food',
-      description: 'Indian buffet',
-      amount: 1000
+      'uuid' => t.uuid,
+      'timestamp' => '2014-01-02',
+      'code' => 'food',
+      'description' => 'Indian buffet',
+      'amount' => 1000
     }
   end
 
-  it 'reconstructs itself from a Hash'
+  it 'reconstructs itself from a Hash' do
+    t = Transaction.from_h({
+      'uuid' => '1234',
+      'timestamp' => '2014-01-02',
+      'code' => 'food',
+      'description' => 'Indian buffet',
+      'amount' => 1500
+    })
+
+    t.uuid.should == '1234'
+    t.timestamp.should == Time.parse('2014-01-02')
+    t.code.should == :food
+    t.description.should == 'Indian buffet'
+    t.amount.should == 1500
+  end
+
   it 'assigns each Transaction a unique identifier'
 end
